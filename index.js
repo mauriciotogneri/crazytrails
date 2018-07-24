7409f2280378b53d9cae2dfb30634a01a179c7e9
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-07-24 23:36:08 +0200

    Testing new code

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.listen(app.get('port'), function() {
 
 //-------------
 
-const WebSocket = require('ws');
+/*const WebSocket = require('ws');
 
-//const ws = new WebSocket('ws://immense-fortress-84995.herokuapp.com/ws');
-//const ws = new WebSocket('ws://localhost/ws');
+const ws = new WebSocket('ws://immense-fortress-84995.herokuapp.com/ws');
+const ws = new WebSocket('ws://localhost/ws');
 
 const ws = new WebSocket.Server({
   port: process.env.PORT
@@ -29,4 +29,35 @@ ws.on('open', function open() {
 
 ws.on('message', function incoming(data) {
   console.log(data);
-});
+});*/
+
+//-------------
+
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
