87d1efbd89b9985f507e85256ca7891887a2a28c
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-07-24 23:55:37 +0200

    Testing new code

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-var WebSocketServer = require('ws').Server
-var http = require('http')
-var express = require('express')
-var app = express()
-var port = process.env.PORT || 5000
-
-app.use(express.static(__dirname + "/"))
-
-var server = http.createServer(app)
-server.listen(port)
-
-var wss = new WebSocketServer({server: server})
+var wss = new WebSocketServer({
+	port: process.env.PORT
+})
 
 wss.on("connection", function(ws)
 {
@@ -20,14 +11,6 @@ wss.on("connection", function(ws)
 
 	ws.on("close", function()
 	{
-		console.log("It's disconnected")
+		console.log("It's disconnected!")
 	})
 })
-
-/*wss.on("connection", function(ws) {
-	ws.send("It's connected")
-
-	ws.on("close", function() {
-		console.log("websocket connection close")
-	})
-})*/
