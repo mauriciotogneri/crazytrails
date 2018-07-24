40dff00be3a82e6a929d4aafeacccf0ffd9054b0
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-07-24 23:48:49 +0200

    Testing new code

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-/*var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello, world!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})*/
-
-//-------------
-
-/*const WebSocket = require('ws')
-
-const ws = new WebSocket('ws://immense-fortress-84995.herokuapp.com/ws')
-const ws = new WebSocket('ws://localhost/ws')
-
-const ws = new WebSocket.Server({
-  port: process.env.PORT
-})
-
-ws.on('open', function open() {
-  ws.send('something')
-})
-
-ws.on('message', function incoming(data) {
-  console.log(data)
-})*/
-
-//-------------
-
 var WebSocketServer = require('ws').Server
 var http = require('http')
 var express = require('express')
@@ -46,7 +11,7 @@ server.listen(port)
 
 var wss = new WebSocketServer({server: server})
 
-wss.on('open', function(ws) {
+/*wss.on('open', function(ws) {
 	console.log("It's connected")
 	ws.send("It's connected")
 
@@ -59,4 +24,17 @@ wss.on('open', function(ws) {
 		console.log("It's disconnected")
 	    ws.send("It's disconnected")
 	})
+})*/
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
 })
