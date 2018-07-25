79ec24da73968f537f5812777b499db10978905a
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-07-25 18:30:45 +0200

    Added web server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var http = require("http")
+var express = require("express")
+var app = express()
+
+app.use(express.static(__dirname + "/public"))
+
+var server = http.createServer(app)
+server.listen(process.env.PORT)
+
+// -------------------------------------------------
+
 const WebSocketServer = require('ws').Server
 const wss = new WebSocketServer({
 	port: process.env.PORT
