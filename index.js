49a52ca7198167954c185d66157eb9aaf411682e
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-07-26 23:54:06 +0200

    Added messages description

--- index.js
+++ index.js
@@ -28,3 +28,121 @@ wss.on("connection", function(ws)
 		console.log("Disconnected")
 	})
 })
+
+/*
+
+CLIENT => SERVER
+
+{
+	command: "create_room"
+	roomName: string
+	playerName: string
+}
+
+{
+	command: "join_room"
+	roomName: string
+	playerName: string
+}
+
+{
+	command: "player_update"
+	key: string
+	vector: Vector
+}
+
+{
+	command: "player_dead"
+	key: string
+}
+
+// ----------------------------------
+
+SERVER => CLIENT
+
+RESPONSE:
+{
+	command: "create_room"
+	roomName: string
+	playerName: string
+	error: string
+}
+
+RESPONSE:
+{
+	command: "join_room"
+	roomName: string
+	playerName: string
+	error: string
+}
+
+BROADCAST:
+{
+	command: "room_closed"
+	players:
+	[
+		{
+			id: int
+			name: string
+			color: string
+			points: int
+			you: bool
+		}
+	]
+	key: string
+}
+
+BROADCAST:
+{
+	command: "prepare_round"
+	players: [
+		{
+			id: int
+			vector: Vector
+		}
+	]
+}
+
+BROADCAST:
+{
+	command: "round_countdown"
+	value: int
+}
+
+BROADCAST:
+{
+	command: "player_update"
+	id: int
+	vector: Vector
+}
+
+BROADCAST:
+{
+	command: "player_dead"
+	id: int
+}
+
+BROADCAST:
+{
+	command: "round_finished"
+	winnerId: int
+	players:
+	[
+		{
+			id: int
+			points: int
+		}
+	]
+}
+
+---------------
+
+Vector: {
+	position: {
+		x: int
+		y: int
+	}
+	direction: UP|DOWN|LEFT|RIGHT
+}
+
+*/
