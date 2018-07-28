d7353b8c61ad1cc56853d6e0ea2eba5c0ae8ae62
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-07-28 23:21:05 +0200

    Sending commands to server and back

--- public/js/input.js
+++ public/js/input.js
@@ -12,19 +12,23 @@ Input.keyDown = function(code)
 {
     if ((code == 38) && (Input.direction != Direction.DOWN))
     {
-        Input.direction = Direction.UP
+        sendMessage(Direction.UP)
+        //Input.direction = Direction.UP
     }
     else if ((code == 40) && (Input.direction != Direction.UP))
     {
-        Input.direction = Direction.DOWN
+        sendMessage(Direction.DOWN)
+        //Input.direction = Direction.DOWN
     }
     else if ((code == 37) && (Input.direction != Direction.RIGHT))
     {
-        Input.direction = Direction.LEFT
+        sendMessage(Direction.LEFT)
+        //Input.direction = Direction.LEFT
     }
     else if ((code == 39) && (Input.direction != Direction.LEFT))
     {
-        Input.direction = Direction.RIGHT
+        sendMessage(Direction.RIGHT)
+        //Input.direction = Direction.RIGHT
     }
 }
 
--- public/js/network.js
+++ public/js/network.js
@@ -1,16 +1,18 @@
-/*var ws = new WebSocket("wss://websocketsserver.herokuapp.com")
+var ws = new WebSocket("wss://websocketsserver.herokuapp.com")
 
 ws.onmessage = function(event)
 {
-    var li = document.createElement('li')
+    /*var li = document.createElement('li')
     li.innerHTML = new String(new Date().getTime() - parseInt(event.data))
-    document.querySelector('#pings').appendChild(li)
+    document.querySelector('#pings').appendChild(li)*/
+    Input.direction = event.data
 }
 
 ws.onopen = function()
 {
-    setInterval(function()
-    {
-        ws.send(new Date().getTime())
-    }, 1000)
-}*/
+}
+
+function sendMessage(data)
+{
+    ws.send(data)
+}
