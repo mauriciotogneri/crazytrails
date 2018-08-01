743eacb54a799a586a922309e497256c33bf439e
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-08-01 11:13:07 +0200

    Adjusted constants

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,9 +1,9 @@
 const DIRECTION_LEFT  = 1
 const DIRECTION_RIGHT = 2
 
-const HEAD_SIZE = 10
+const HEAD_SIZE = 8
 
 const FPS = 30
 
-const DISTANCE_RATE = (60/FPS) * 150
-const ANGLE_RATE    = (60/FPS) * 150
+const DISTANCE_RATE = (60/FPS) * 120
+const ANGLE_RATE    = (60/FPS) * 160
