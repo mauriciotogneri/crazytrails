58ec279a65fae6255bbe2f0f6e5dd48d4b2649ce
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-08-03 16:41:05 +0200

    Sending last 10 positions

--- public/js/worm-local.js
+++ public/js/worm-local.js
@@ -21,15 +21,44 @@ class WormLocal extends Worm
 
     sendPositionUpdate()
     {
-        const buffer = new ArrayBuffer(1 + 1 + 1 + 1 + 4 + 4 + 4)
+        const last = this.lastPositions()
+
+        const buffer = new ArrayBuffer(1 + 1 + 1 + 1 + 4 + (last.length * 4 * 2))
         
         const view = new DataView(buffer)
         view.setUint8(0, this.drawing ? 1 : 0)
         view.setUint8(1, this.direction)
+        view.setUint8(2, last.length)
+        view.setUint8(3, 0) // unused
+
         view.setFloat32(4, this.angle)
-        view.setFloat32(8, this.head.x)
-        view.setFloat32(12, this.head.y)
+        
+        var index = 8
+
+        last.forEach(point =>
+        {
+            view.setFloat32(index,     point.x)
+            view.setFloat32(index + 4, point.y)
+
+            index += 8
+        })
 
         Network.send(buffer)
+    }
+
+    lastPositions()
+    {
+        var result = []
+
+        if (this.path.segments.length < 10)
+        {
+            result = this.path.segments
+        }
+        else
+        {
+            result = this.path.segments.slice(this.path.segments.length - 10, this.path.segments.length)
+        }
+
+        return result.map(s => s.point)
     }
 }
