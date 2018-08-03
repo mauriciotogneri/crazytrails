b524738b991bd8a60d9dc891ec845e594a99aebe
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-08-03 23:40:29 +0200

    Code cleanup

--- public/js/worm-remote.js
+++ public/js/worm-remote.js
@@ -37,43 +37,5 @@ class WormRemote extends Worm
         {
             this.startDrawing()
         }
-        
-        /*if (this.drawing)
-        {
-            const closest = this.closestSegment(this.head.x, this.head.y)
-
-            if (closest != -1)
-            {
-                this.path.removeSegments(closest)
-            }
-            else
-            {
-                this.removeLastSegment()
-            }
-
-            this.path.add(this.head)
-        }*/
     }
-
-    /*closestSegment(x, y)
-    {
-        var index = -1
-        var min   = 2
-
-        const point = new paper.Point([x, y])
-        const segments = this.path.segments
-
-        for (var i = segments.length - 20; (i >= 0) && (i < segments.length); i++)
-        {
-            var distance = segments[i].point.getDistance(point)
-
-            if (distance < min)
-            {
-                min = distance
-                index = i
-            }
-        }
-
-        return index
-    }*/
 }
