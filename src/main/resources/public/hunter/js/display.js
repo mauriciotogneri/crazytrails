0abd765f9812d233dff8fecb3cd4a4c144db1c34
Mauricio Togneri <mauricio.togneri@gmail.com>
2018-08-27 23:05:41 +0200

    Playing with textures

--- src/main/resources/public/hunter/js/display.js
+++ src/main/resources/public/hunter/js/display.js
@@ -8,7 +8,7 @@ class Display
         this.camera.lookAt(new THREE.Vector3(0,0,0))
 
         // first person view
-        //this.camera.up.set(0, 1, 0)
+        this.camera.up.set(0, 1, 0)
 
         this.renderer = new THREE.WebGLRenderer({canvas: $("#canvas"), antialias: true})
         this.renderer.setClearColor(0x222222)
@@ -46,34 +46,31 @@ class Display
         })
 
         // orbit controls
-        //this.controls.update()
+        //his.controls.update()
+
         this.renderer.render(this.scene, this.camera)
     }
 
     centerAt(x, y)
     {
         // uncomment to use orbit
-        this.camera.position.x = x
-        this.camera.position.y = y
+        this.camera.position.set(x, y, -50)
 
         // first person view
-        //this.camera.position.set(x, y, 0)
-        //this.camera.lookAt(new THREE.Vector3(x, y - 50, 0))
+        this.camera.lookAt(new THREE.Vector3(x, y - 50, -50))
     }
 
-    cube(x, y, z, a, b, c, texture)
+    cube(x, y, z, a, b, c, textureName)
     {
         const geometry = new THREE.BoxGeometry(a, b, c)
         // use MeshFaceMaterial and pass an array with 6 MeshBasicMaterial to paint each face differently
 
-        var texture2 = new THREE.TextureLoader().load(texture, function(texture)
-        {
-            texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-            texture.offset.set( 0, 0 )
-            texture.repeat.set( 2, 2 )
-        })
+        const texture = new THREE.ImageUtils.loadTexture(textureName)
+        texture.wrapS = THREE.RepeatWrapping
+        texture.wrapT = THREE.RepeatWrapping
+	    texture.repeat.set(1, 10, 0)
 
-        const material = new THREE.MeshLambertMaterial({map: texture2})
+        const material = new THREE.MeshLambertMaterial({map: texture})
         const mesh = new THREE.Mesh(geometry, material)
         mesh.position.set(x, y, z)
 
